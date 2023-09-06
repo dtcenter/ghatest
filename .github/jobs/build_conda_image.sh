8990ab61c090c3f547fcaa5f5d255d815cc6397f
George McCabe <23407799+georgemccabe@users.noreply.github.com>
2023-09-06 14:50:37 -0600

    2 repos

--- .github/jobs/build_conda_image.sh
+++ .github/jobs/build_conda_image.sh
@@ -1,4 +1,4 @@
-cd internal/scripts/docker_env
+cd ${GITHUB_WORKSPACE}/METplus/internal/scripts/docker_env
 echo docker build -t dtcenter/metplus-envs:conda.${METPLUS_ENV_VERSION} \
   -f Dockerfile.conda .
 docker build -t dtcenter/metplus-envs:conda.${METPLUS_ENV_VERSION} \
